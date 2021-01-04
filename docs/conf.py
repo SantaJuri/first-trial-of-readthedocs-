46bad1fa0726998afb028e09990147feed96b8bd
paulkuehnel_hps <paul.kuehnel@homepowersolutions.de>
2021-01-04 11:19:01 +0100

    increased version number

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Paul K.'
 author = 'Paul K.'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
